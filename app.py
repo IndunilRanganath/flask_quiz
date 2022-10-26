b1e459bfc38c56faa6cf2e17e86ca821254d9ef7
IndunilRanganath <indunilranganath071@gmail.com>
2022-10-26 11:29:59 +0530

    created navbar

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+# 4.38
+
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello Flask</h1>'
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
