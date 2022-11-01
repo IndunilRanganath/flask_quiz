a9d62991502504fc9432aa1314aacc8210b1ac4e
IndunilRanganath <indunilranganath071@gmail.com>
2022-11-01 17:33:05 +0530

    created home page

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-# 4.38
+# 4.12
 
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template("home.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/logout')
+def logout():
     return render_template("home.html")
 
 if __name__ == "__main__":
